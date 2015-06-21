190c982447231f57929f5a14c620e2071122c578
Kevin Kuhlmann <kevinkuhlmann0526@gmail.com>
2015-06-21 16:16:53 -0400

    commiting changes

--- crm.rb
+++ crm.rb
@@ -6,7 +6,7 @@ $rolodex = Rolodex.new
 
 get '/' do
   @crm_app_name = "My CRM"
-  erb :index
+  erb :homepage
 end
 
 get "/contacts" do
@@ -21,5 +21,24 @@ end
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:notes])
   $rolodex.add_contact(new_contact)
-  redirect to('/contacts')
+  redirect to('/')
 end
+
+
+# get '/contacts/new' do
+#   puts
+#   puts "params => #{params}"
+#   puts
+
+#   "GET request: #{params}"
+
+# end
+
+# post '/contacts/new' do
+#   puts
+#   puts "params => #{params}"
+#   puts
+
+#   "POST request: #{params}"
+
+# end
